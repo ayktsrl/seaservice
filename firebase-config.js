a2772d69bd7c12a71e4a606e67380fc75ab0bf87
ayktsrl <aykutsaral@gmail.com>
2025-07-04 21:02:44 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -7,11 +7,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 const firebaseConfig = {
   apiKey: "AIzaSyDuF_HlZYG0v7SUaSzILV584J8b5ybnBUU",
   authDomain: "crewapplication-c37a8.firebaseapp.com",
+  databaseURL: "https://crewapplication-c37a8-default-rtdb.firebaseio.com",
   projectId: "crewapplication-c37a8",
   storageBucket: "crewapplication-c37a8.appspot.com",
   messagingSenderId: "23423243284",
   appId: "1:23423243284:web:67e22f6ed057c74f4b4301"
-  // measurementId: "G-DSSJMESQ7Z" // Özellikle Analytics kullanıyorsanız uncomment edin
+  // measurementId: "G-DSSJMESQ7Z" // Uncomment if using Analytics
 };
 
 // Initialize Firebase
