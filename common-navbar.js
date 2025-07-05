56973ab659a4a170ab2a8aa61cbe2a93252f125b
ayktsrl <aykutsaral@gmail.com>
2025-07-05 21:33:10 +0300

    Add files via upload

--- common-navbar.js
+++ common-navbar.js
@@ -1,8 +1,6 @@
 (function() {
-  // Derinlik tespiti (kaç klasör yukarı çıkmalıyız?)
   function getBasePath() {
     const path = window.location.pathname;
-    // Örnek: /Agency/agency-job-list.html -> 1 yukarı çıkmalı: '../'
     const match = path.match(/\/(Agency|Company|Seafarer)\//i);
     if (match) return '../';
     return '';
@@ -13,7 +11,7 @@
   const navLinks = document.getElementById('navLinks');
   const dashboardBrand = document.getElementById('dashboardBrand');
   let menuHtml = '';
-  let dashboardHref = base + 'home.html';
+  const dashboardHref = base + 'home.html'; // HER ZAMAN home.html
 
   if (role === 'company') {
     menuHtml += `
@@ -25,25 +23,21 @@
       <li class="nav-item"><a class="nav-link" href="${base}Company/evaluate-seafarers.html">Evaluate Seafarers</a></li>
       <li class="nav-item"><a class="nav-link" href="${base}Company/company-verify-seaservice.html">Verify Sea Service</a></li>
     `;
-    dashboardHref = base + 'Company/company-applications.html';
   } else if (role === 'agency') {
     menuHtml += `
       <li class="nav-item"><a class="nav-link" href="${base}Agency/agency-post-job.html">Post Job</a></li>
       <li class="nav-item"><a class="nav-link" href="${base}Agency/agency-job-list.html">My Jobs</a></li>
       <li class="nav-item"><a class="nav-link" href="${base}Agency/agency-seafarers.html">Seafarers</a></li>
     `;
-    dashboardHref = base + 'Agency/agency-job-list.html';
   } else if (role === 'seafarer') {
     menuHtml += `
       <li class="nav-item"><a class="nav-link" href="${base}Seafarer/my-applications.html">My Applications</a></li>
       <li class="nav-item"><a class="nav-link" href="${base}Seafarer/edit-cv-panel.html">Edit CV</a></li>
     `;
-    dashboardHref = base + 'Seafarer/my-applications.html';
   } else {
     menuHtml += `
       <li class="nav-item"><a class="nav-link" href="${base}login.html">Login</a></li>
     `;
-    dashboardHref = base + 'home.html';
   }
 
   navLinks.innerHTML = menuHtml;
@@ -60,12 +54,10 @@
     `;
   }
 
-  // Optionally set username
   const usernameDisplay = document.getElementById('usernameDisplay');
   const username = localStorage.getItem('username');
   if (usernameDisplay && username) usernameDisplay.textContent = username;
 
-  // Logout logic
   const logoutBtn = document.getElementById('logoutBtn');
   if (logoutBtn) {
     logoutBtn.addEventListener('click', function() {
