281ec4d652730d609f909490ba1cf26d83cbb282
ayktsrl <aykutsaral@gmail.com>
2025-07-05 21:25:26 +0300

    Add files via upload

--- common-navbar.js
+++ common-navbar.js
@@ -1,54 +1,77 @@
 (function() {
+  // Derinlik tespiti (kaç klasör yukarı çıkmalıyız?)
+  function getBasePath() {
+    const path = window.location.pathname;
+    // Örnek: /Agency/agency-job-list.html -> 1 yukarı çıkmalı: '../'
+    const match = path.match(/\/(Agency|Company|Seafarer)\//i);
+    if (match) return '../';
+    return '';
+  }
+  const base = getBasePath();
+
   const role = localStorage.getItem('role');
   const navLinks = document.getElementById('navLinks');
   const dashboardBrand = document.getElementById('dashboardBrand');
   let menuHtml = '';
-  let dashboardHref = 'home.html';
+  let dashboardHref = base + 'home.html';
 
   if (role === 'company') {
     menuHtml += `
-      <li class="nav-item"><a class="nav-link" href="Company/company-applications.html">Job Applications</a></li>
-      <li class="nav-item"><a class="nav-link" href="Company/create-job.html">Create Job</a></li>
-      <li class="nav-item"><a class="nav-link" href="Company/company-ship-list.html">Ships</a></li>
-      <li class="nav-item"><a class="nav-link" href="Company/ship-crew-list.html">Crew List</a></li>
-      <li class="nav-item"><a class="nav-link" href="Company/public-cv-browser.html">CV Browser</a></li>
-      <li class="nav-item"><a class="nav-link" href="Company/evaluate-seafarers.html">Evaluate Seafarers</a></li>
-      <li class="nav-item"><a class="nav-link" href="Company/company-verify-seaservice.html">Verify Sea Service</a></li>
+      <li class="nav-item"><a class="nav-link" href="${base}Company/company-applications.html">Job Applications</a></li>
+      <li class="nav-item"><a class="nav-link" href="${base}Company/create-job.html">Create Job</a></li>
+      <li class="nav-item"><a class="nav-link" href="${base}Company/company-ship-list.html">Ships</a></li>
+      <li class="nav-item"><a class="nav-link" href="${base}Company/ship-crew-list.html">Crew List</a></li>
+      <li class="nav-item"><a class="nav-link" href="${base}Company/public-cv-browser.html">CV Browser</a></li>
+      <li class="nav-item"><a class="nav-link" href="${base}Company/evaluate-seafarers.html">Evaluate Seafarers</a></li>
+      <li class="nav-item"><a class="nav-link" href="${base}Company/company-verify-seaservice.html">Verify Sea Service</a></li>
     `;
-    dashboardHref = 'Company/company-applications.html';
+    dashboardHref = base + 'Company/company-applications.html';
   } else if (role === 'agency') {
     menuHtml += `
-      <li class="nav-item"><a class="nav-link" href="Agency/agency-post-job.html">Post Job</a></li>
-      <li class="nav-item"><a class="nav-link" href="Agency/agency-job-list.html">My Jobs</a></li>
-      <li class="nav-item"><a class="nav-link" href="Agency/agency-seafarers.html">Seafarers</a></li>
+      <li class="nav-item"><a class="nav-link" href="${base}Agency/agency-post-job.html">Post Job</a></li>
+      <li class="nav-item"><a class="nav-link" href="${base}Agency/agency-job-list.html">My Jobs</a></li>
+      <li class="nav-item"><a class="nav-link" href="${base}Agency/agency-seafarers.html">Seafarers</a></li>
     `;
-    dashboardHref = 'Agency/agency-job-list.html';
+    dashboardHref = base + 'Agency/agency-job-list.html';
   } else if (role === 'seafarer') {
     menuHtml += `
-      <li class="nav-item"><a class="nav-link" href="Seafarer/my-applications.html">My Applications</a></li>
-      <li class="nav-item"><a class="nav-link" href="Seafarer/edit-cv-panel.html">Edit CV</a></li>
+      <li class="nav-item"><a class="nav-link" href="${base}Seafarer/my-applications.html">My Applications</a></li>
+      <li class="nav-item"><a class="nav-link" href="${base}Seafarer/edit-cv-panel.html">Edit CV</a></li>
     `;
-    dashboardHref = 'Seafarer/my-applications.html';
+    dashboardHref = base + 'Seafarer/my-applications.html';
   } else {
     menuHtml += `
-      <li class="nav-item"><a class="nav-link" href="login.html">Login</a></li>
+      <li class="nav-item"><a class="nav-link" href="${base}login.html">Login</a></li>
     `;
-    dashboardHref = 'home.html';
+    dashboardHref = base + 'home.html';
   }
 
-  if (navLinks) navLinks.innerHTML = menuHtml;
-  if (dashboardBrand) dashboardBrand.setAttribute('href', dashboardHref);
+  navLinks.innerHTML = menuHtml;
+  dashboardBrand.setAttribute('href', dashboardHref);
 
+  // User dropdown linkleri role ve derinliğe göre ayarlanıyor:
+  const userMenuLinks = document.getElementById('userMenuLinks');
+  if (userMenuLinks) {
+    userMenuLinks.innerHTML = `
+      <li><a class="dropdown-item" href="${base}edit-profile.html">Edit Profile</a></li>
+      <li><a class="dropdown-item" href="${base}change-password.html">Change Password</a></li>
+      <li><hr class="dropdown-divider"></li>
+      <li><a class="dropdown-item" href="#" id="logoutBtn">Logout</a></li>
+    `;
+  }
+
+  // Optionally set username
   const usernameDisplay = document.getElementById('usernameDisplay');
   const username = localStorage.getItem('username');
   if (usernameDisplay && username) usernameDisplay.textContent = username;
 
+  // Logout logic
   const logoutBtn = document.getElementById('logoutBtn');
   if (logoutBtn) {
     logoutBtn.addEventListener('click', function() {
       localStorage.removeItem('role');
       localStorage.removeItem('username');
-      window.location.href = 'index.html';
+      window.location.href = base + 'index.html';
     });
   }
 })();
