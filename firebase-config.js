7f6b734b84a0f71345f82eb89ea80303d46927cd
ayktsrl <aykutsaral@gmail.com>
2025-08-30 20:33:40 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 // firebase-config.js
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth       } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore  } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth }       from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore }  from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// Senin proje ayarların (değiştirmedim)
 const firebaseConfig = {
   apiKey:            "AIzaSyDuF_HlZYG0v7SUaSzILV584J8b5ybnBUU",
   authDomain:        "crewapplication-c37a8.firebaseapp.com",
@@ -11,12 +12,10 @@ const firebaseConfig = {
   storageBucket:     "crewapplication-c37a8.appspot.com",
   messagingSenderId: "23423243284",
   appId:             "1:23423243284:web:67e22f6ed057c74f4b4301"
-  // measurementId:   "G-DSSJMESQ7Z" // Analytics kullanıyorsan açabilirsin
+  // measurementId:   "G-DSSJMESQ7Z" // Analytics istersen açabilirsin
 };
 
 // Initialize Firebase
-const app  = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db   = getFirestore(app);
-
-export { app, auth, db };
+export const app  = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db   = getFirestore(app);
